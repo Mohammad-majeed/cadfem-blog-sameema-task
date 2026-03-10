f4ee484ad1365250fb0cf1cb56305ba4d8940c79
Mohammad-majeed <105845339+Mohammad-majeed@users.noreply.github.com>
2026-03-10 23:10:09 +0530

    Update cad-blog.js

--- cad-blog.js
+++ cad-blog.js
@@ -1,6 +1,17 @@
+// Reading Progress Bar logic
 window.onscroll = function() {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrolled = (winScroll / height) * 100;
     document.getElementById("progress-bar").style.width = scrolled + "%";
 };
+
+// Smooth scroll for TOC links
+document.querySelectorAll('.toc-list a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
